db7b7055a06b14f999ddde0aa09ff8d3663d2c88
Reed Keyes <rbkeyes@gmail.com>
2022-11-25 22:17:08 -0800

    working on color coding by past/present/future

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 https://github.com/EdenKhaos/05-work-day-scheduler
 
+https://github.com/JulieAnn2330/work-day-scheduler/blob/master/index.html
+
+
+jQuery documentation:
+[.click()](https://api.jquery.com/click/)
+
+[.siblings()](https://api.jquery.com/siblings/)
+
+[.toggle()](https://api.jquery.com/toggleClass/)
+
+[.attr()](https://api.jquery.com/attr/)
+
+w3schools:
+[w3schools "this"](https://www.w3schools.com/js/js_this.asp)
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,66 +1,161 @@
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// add timeblocks for 9-5 in HTML
-var scheduleRow = $('.row');
-var timeBlock = $('time-block');
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// get variables past/present/future
-var pastTasks = $('.past');
-var presentTasks = $('.present');
-var futureTasks = $('.future');
-
-
-// link to day.js current time
-// function to change color for < current time, == current time, > current time
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// add text to text area
-// get variable textarea
-
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// get save button element
-
-// WHEN I refresh the page
-// THEN the saved events persist 
-// get & display saved events from local storage at page open
-
-
-// $(document).ready() function
-// shorthand = $(function(){})
-// prevents code being read until DOM is ready
+// document.ready function to prevent action until DOM is ready
 $(function () {
     console.log('ready to go!')
 
-    // today's date, display in header
-    
-    var today = dayjs();
-    $('#currentDay').text(today.format('MMMM D, YYYY h:mm a'));
-    
-    var textInput = $('.description');
+    // display date
+    const date = dayjs().format('MMMM D, YYYY');
+    $("#currentDay").text(date);
 
-        // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-        // HINT: What does `this` reference in the click listener function? 
-        // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
-        // How might the id be useful when saving the description in local storage?
-});
+    let task = "";
+    let hour = "";
 
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+// add event listener to save btn
+$(".saveBtn").click(function () {
+
+    // get value of the element with class of "description" (sibling to button)
+    task = $(this).siblings(".description").val(); 
+    console.log(this);
+    // set variable hour to id arribute of div element (parent to button)
+    hour = $(this).parent().attr("id");
+    //set items in local storage.
+    // key=hour
+    // value=task
+    localStorage.setItem(hour, JSON.stringify(task));
+
+    console.log(hour);
+    console.log(task);
+} );
+
+// getFromLocalStorage();
+function displaySavedOnPage() {
+    JSON.parse(localStorage.getItem(hour));
+
+      } 
+
+
+// color code time blocks by past/present/future
+function pastPresentFuture() {
+    // current time
+    const now = dayjs().hour();
+    console.log(now);
+    if (typeof now === Number);
+    console.log("number");
+
+    var schedule = $('.time-block');
+    console.log(schedule);
+
+    for (var i=0; i< schedule.length;i++) {
+        var scheduleBlock = $(schedule[i]);
+        console.log(scheduleBlock);
+
+        var blockID = scheduleBlock.attr("id");
+        console.log(blockID);   
+        if (typeof blockID === Number);
+        console.log("number");
+        
+    // var past = (blockID < now);
+    var present = (blockID == now);
+    console.log(present);
+    var future = (blockID > now);
+    console.log(future);
+
+    if (blockID < now) {
+        console.log("past");
+        console.log(this.schedule);
+
+    }
+} 
+
+pastPresentFuture();
+
+displaySavedOnPage();
+}
+}
+);
+
+// function colorCodeHours() {
+//     var allCards = $('.time-block');
+//     for (let i = 0; i < allCards.length; i++) {
+//       var currentCard = $(allCards[i]);
+//       var currentCardID = currentCard.attr('id');
+//       var isCurrentHour = currentCardID == currentHourID;
+//       var isCurrentBlock = currentCard.is(".present");
+//       var hourIsChanged = isCurrentHour && !isCurrentBlock
+//       if (hourIsChanged) {
+//         var oldPresentCard = $('.present');
+//         oldPresentCard.addClass('past');
+//         oldPresentCard.removeClass('present');
+//         currentCard.addClass('present');
+//         currentCard.removeClass('future');
+//       }
+//     }
+//   }
+
+
+    // get saved tasks from local storage
+    // function getFromLocalStorage() {
+    // get each time block element
+  
+// function getSavedTasks() {
+// }
+// var schedule = $('.time-block');   
+// console.log(schedule);
+//     // get each time block element ID
+    
+//     // loop through to retrieve all saved tasks
+// for (var i=0; i<schedule.length; i++) {
+//     let scheduleItem = schedule[i];
+//         console.log(scheduleItem);
+// }
+
+
+
+
+
+
+
+
+
+
+// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+// var timeBlock = $('time-block');
+// var taskInput = $('textarea');
+// var pastTasks = $('.past');
+// var presentTasks = $('.present');
+// var futureTasks = $('.future');
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+
+// 
+// }
+
+
+
+  // display tasks to appropriate timeblock on page
+// function displayTasks() {
+//     // clear current projects on the page
+//     projectDisplayEl.empty();
+  
+//     // get projects from localStorage
+    // var projects = readProjectsFromStorage();
+  
+  
+      
+      // add class to row by comparing project date to today's date
+//       if (projectDate.isBefore(today)) {
+//         rowEl.addClass('project-late');
+//       } else if (projectDate.isSame(today)) {
+//         rowEl.addClass('project-today');
+//       }
+  
+//       // append elements to DOM to display them
+//       rowEl.append(nameEL, typeEl, dateEl, deleteEl);
+//       projectDisplayEl.append(rowEl);
+//     }
+//   }
+
+
+// }
 
